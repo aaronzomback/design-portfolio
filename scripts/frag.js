096494c25057eafd16d86a4d715f4fa45b38e8ae
aaronzomback <aaronzomback@gmail.com>
2021-10-14 23:11:53 +0200

    add github to header

--- scripts/frag.js
+++ scripts/frag.js
@@ -2,16 +2,11 @@ const frag = `
 #ifdef GL_ES
 precision highp float;
 #endif
-
 uniform float u_time;
-
-
 varying vec2 v_texcoord;
-
 vec4 rgb(float r, float g, float b) {
     return vec4(r / 255.0, g / 255.0, b / 255.0, 1.0);
 }
-
 void main(void)
 {
     vec2 uv = v_texcoord;
@@ -20,7 +15,6 @@ void main(void)
     vec4 tr = rgb(222.0, 225.0, 255.0);
     vec4 bl = rgb(54.0, 72.0, 235.0);
     vec4 br = rgb(255.0, 255.0, 255.0);
-
     
     float dispY = 0.09 * sin(u_time);
     float dispX = 0.03 * sin(u_time);
